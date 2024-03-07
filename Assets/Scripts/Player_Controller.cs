7ae55b584a1cc13df0a158f4c09e5c59a14585b3
SkyWalker-2001 <126860277+SkyWalker-2001@users.noreply.github.com>
2024-03-07 12:37:50 +0530

    Facing Right and Flip

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -8,8 +8,13 @@ public class Player_Controller : MonoBehaviour
     [SerializeField] private float _jumpForce;
 
     private Rigidbody2D _rb;
+    private Animator _player_AnimatorController;
 
     private float _movingInput;
+    private bool canDoubleJump = true;
+
+    private bool _facingRight = true;
+    private int _facindDirection = 1;
 
     [Header("RayCast Ground")]
 
@@ -22,25 +27,78 @@ public class Player_Controller : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _player_AnimatorController = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        AnimationController();
+
         Collisiion_Check();
 
+        Input_Checks();
+
+        if (_isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        Move();
+       
+        Flip_Controller();
+    }
+
+    private void AnimationController()
+    {
+        bool isMoving = _rb.velocity.x != 0;
+
+        _player_AnimatorController.SetBool("isMoving", isMoving);
+        _player_AnimatorController.SetBool("isGrounded", _isGrounded);
+        _player_AnimatorController.SetFloat("yVelocity", _rb.velocity.y);
+    }
+
+    private void Input_Checks()
+    {
         _movingInput = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (_isGrounded)
-            {
-                Jump();
-            }
+            Jump_Button();
         }
-
-        Move();
     }
 
+    private void Flip_Controller()
+    {
+        if(_facingRight && _movingInput < 0)
+        {
+            Flip();
+        }
+        else if (!_facingRight && _movingInput > 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        _facindDirection = _facindDirection * -1;
+        _facingRight = !_facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void Jump_Button()
+    {
+        if (_isGrounded)
+        {
+            Jump();
+        }
+        else if (canDoubleJump)
+        {
+            canDoubleJump = false;
+            Jump();
+        }
+    }
+        
     private void Move()
     {
         _rb.velocity = new Vector2(_movingInput * _moveSpeed, _rb.velocity.y);
