c8c3fe9d33e23e005f47858cbf6e61eae68f3295
SkyWalker-2001 <126860277+SkyWalker-2001@users.noreply.github.com>
2024-03-29 23:14:06 +0530

    bluebird enemy

---
+++ Assets/Scripts/Enemies/BlueBird_Enemy.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueBird_Enemy : Enemy
+{
+    private RaycastHit2D ceillingDetected;
+
+    [Header("BlueBird Info")]
+    [SerializeField] private float ceillingDistance;
+    [SerializeField] private float groundDistance;
+
+    [SerializeField] private float flyUpForce;
+    [SerializeField] private float flyDownForce;
+                     private float flyForce;
+
+    private bool canFly = true;
+
+    public override void Damage()
+    {
+        canFly = false;
+        rb.velocity = Vector2.zero;
+        rb.gravityScale = 0;
+        base.Damage();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        flyForce = flyUpForce; 
+    }
+
+    private void Update()
+    {
+        CheckCollision();
+
+        if (ceillingDetected)
+        {
+            flyForce = flyDownForce;
+        }
+
+        else if(groundDetected)
+        {
+            flyForce = flyUpForce;
+        }
+
+        if (wallDetected)
+        {
+            Flip();
+        }
+    }
+
+    public void FlyUpEvent()
+    {
+        if(canFly)
+            rb.velocity = new Vector2(speed * _facingDirection, flyForce);
+    }
+
+    protected override void CheckCollision()
+    {
+        base.CheckCollision();
+
+        ceillingDetected = Physics2D.Raycast(transform.position, Vector2.up, ceillingDistance, whatIsGround);
+    }
+
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y + ceillingDistance));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundDistance));
+    }
+}
