27fd46ddc70bd68f00e9a8b4907db566e087e217
SkyWalker-2001 <126860277+SkyWalker-2001@users.noreply.github.com>
2024-04-18 23:22:22 +0530

    Trump_COMP

--- Assets/Scripts/Enemies/Trunk_Enemy.cs
+++ Assets/Scripts/Enemies/Trunk_Enemy.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 public class Trunk_Enemy : Enemy
 {
     [Header("Trunk spec")]
+   [SerializeField] private float retreatBackTime;
+   private float retreatTimeCounter;
+
+    [Header("Collision system")]
     [SerializeField] private float checkRadius;
     [SerializeField] private LayerMask whatIsPlayer;
+    [SerializeField] private Transform groundBehindCheck;
+    private bool wallBehind;
+    private bool groundBehind;
 
     private bool checkPlayer;
 
@@ -31,7 +38,10 @@ public class Trunk_Enemy : Enemy
         }
 
         attackCoolDownCounter -= Time.deltaTime;
+        retreatTimeCounter -= Time.deltaTime;
 
+        if(playerDetection && retreatTimeCounter < 0)
+            retreatTimeCounter = retreatBackTime;
         if (playerDetection.collider.GetComponent<Player_Controller>() != null)
         {
             if (attackCoolDownCounter < 0)
@@ -40,16 +50,32 @@ public class Trunk_Enemy : Enemy
                 anim.SetTrigger("attack");
                 canMove = false;
             }
+            else if (playerDetection.distance < 3)
+            {
+                MoveBackwards(1.5f);
+            }
         }
         else
         {
+            if(retreatTimeCounter > 0)
+                MoveBackwards(4);
+
             WalkAround();
         }
 
         anim.SetFloat("xVelocity", rb.velocity.x);
 
     }
+    private void MoveBackwards(float multiplier)
+    {
+        if (wallBehind)
+            return;
 
+        if (!groundBehind)
+            return;
+
+        rb.velocity = new Vector2(speed * multiplier * -_facingDirection, rb.velocity.y);
+    }
     private void AttackEvent()
     {
         GameObject newBullet = Instantiate(bullet_Prefab, bulletOriginPoint.transform.position, bulletOriginPoint.transform.rotation);
@@ -67,5 +93,15 @@ public class Trunk_Enemy : Enemy
         base.CheckCollision();
 
         checkPlayer = Physics2D.OverlapCircle(transform.position, checkRadius, whatIsPlayer);
+        groundBehind = Physics2D.Raycast(groundBehindCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        wallBehind = Physics2D.Raycast(wallCheck.position, Vector2.right * (-_facingDirection + 1), wallCheckDistance, whatIsGround);
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+
+        Gizmos.DrawWireSphere(transform.position, checkRadius);
+        Gizmos.DrawLine(groundBehindCheck.position, new Vector2(groundBehindCheck.position.x, groundBehindCheck.position.y - groundCheckDistance));
     }
 }
